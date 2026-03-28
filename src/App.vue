57cd6a674582048facda0c059f64ee26070db229
vance <vanci_g@163.com>
2026-03-28 12:55:10 +0800

    Update: cotv21.2-pro project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <RegisterPage />
+  </div>
+</template>
+
+<script setup>
+import RegisterPage from './components/RegisterPage.vue'
+</script>
+
+<style scoped>
+.app {
+  width: 100%;
+}
+</style>
